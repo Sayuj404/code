384a734377e1286e12edec98d0bfea60d139af5b
sayuj404 <2023becs076@mite.ac.in>
2024-11-14 10:24:55 +0530

    new

---
+++ inedx.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int i, a, b;
+    printf("Enter the 1st number: ");
+    scanf("%d", &a);
+
+    printf("Enter the 2nd number: ");
+    scanf("%d", &b);
+
+    printf("Numbers are: %d and %d", a, b);
+    return 0;
+}
